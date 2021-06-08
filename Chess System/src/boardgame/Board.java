fa39ef1fed742b2688ecabbd8510d441d54e8084
camila1467 <camila3467@outlook.com>
2021-06-08 19:44:33 -0300

    BoardException and defensive programing

--- Chess System/src/boardgame/Board.java
+++ Chess System/src/boardgame/Board.java
@@ -6,8 +6,11 @@ private int columns;
 private Piece[][]pieces;
 
 public Board(int rows, int columns) {
-	super();
-	this.rows = rows;
+if (rows<1|| columns <1) {
+	throw new BoardException("error creating board:there must be at least 1 row and 1 column");
+}
+
+this.rows = rows;
 	this.columns = columns;
 	pieces= new Piece [rows][columns];
 }
@@ -16,17 +19,13 @@ public int getRows() {
 	return rows;
 }
 
-public void setRows(int rows) {
-	this.rows = rows;
-}
+
 
 public int getColumns() {
 	return columns;
 }
 
-public void setColumns(int columns) {
-	this.columns = columns;
-}
+
 
 public Piece[][] getPieces() {
 	return pieces;
@@ -37,19 +36,47 @@ public void setPieces(Piece[][] pieces) {
 }
 //retorna a matriz pieces
 public Piece piece(int row, int column ) {
+	if (!positionExists(row, column)) {
+		throw new BoardException ("position mot on the board");
+	}
 	return pieces[row][column];
 	
 }
 //retorna a peca pela posicao
 public Piece piece(Position position ) {
+	if (!positionExists(position)) {
+		throw new BoardException ("position not on the board");
+	}
+	
 	return pieces[position.getRow()][position.getColumn()];
 	
 }
 public void placePiece(Piece piece,Position position ) {
+	if (thereIsAPiece(position)) {
+		throw new BoardException ("There is alredy a piece on position"+position);
+
+	}
 	 pieces[position.getRow()][position.getColumn()]=piece;
 	 piece.position=position;
 	
 }
 
+private boolean positionExists (int row, int column) {
+	return row>=0 && row< rows && column >=0 && column<columns;
+	
+}
+
+
+public boolean positionExists(Position position) {
+	return  positionExists(position.getRow(),position.getColumn());
+	
+}
+public boolean thereIsAPiece (Position position) {
+	if (!positionExists(position)) {
+		throw new BoardException ("position not on the board");
+	}
+	return piece(position)!=null;
+	
+}
 
 }
