3b472aec1b48a4cc27956b376f6c69754bfa0aad
camila1467 <camila3467@outlook.com>
2021-06-07 21:47:25 -0300

    Starting to Implement Board Piece

--- Chess System/src/apllication.java
+++ Chess System/src/apllication.java
@@ -1,11 +1,10 @@
-import boardgame.Position;
+import boardgame.Board;
 
 public class apllication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Position pos= new Position (3,5);
-System.out.println(pos);
+Board board = new Board(8,8);
 	}
 
 }
