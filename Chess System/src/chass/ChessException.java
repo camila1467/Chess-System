035d373333e02bc6d2f1a1d5b7b107bb10356f0e
camila1467 <camila3467@outlook.com>
2021-06-08 20:10:37 -0300

    ChessException and ChessPosition

---
+++ Chess System/src/chass/ChessException.java
@@ -0,0 +1,20 @@
+package chass;
+
+public class ChessException  extends RuntimeException {
+	private static final long serialVersionUID=1L;
+	public ChessException(String msg) {
+		super(msg);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
