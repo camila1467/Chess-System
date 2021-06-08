cecef67e67af342d93af50f040cb2d6fefb2b039
camila1467 <camila3467@outlook.com>
2021-06-07 21:28:41 -0300

    fisrt class:Position

--- Chess System/src/apllication.java
+++ Chess System/src/apllication.java
@@ -1,9 +1,11 @@
+import boardgame.Position;
 
 public class apllication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("ca");
+Position pos= new Position (3,5);
+System.out.println(pos);
 	}
 
 }
